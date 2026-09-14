c1d40bad0dce2b3f05d8aa40df5e1b288fbfd76d
Herman Faustino <ismaelsapilinha3@gmail.com>
2026-09-14 15:56:28 +0100

    criação do first-component

---
+++ SGE/src/app/components/first-component/first-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-first-component',
+  styleUrl: './first-component.css',
+  templateUrl: './first-component.html',
+})
+export class FirstComponent {}
