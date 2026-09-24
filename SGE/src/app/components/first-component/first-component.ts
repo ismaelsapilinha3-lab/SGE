12733503b45dcff461da41c89bce047e9ae3de03
Délcio Domingos <delciodominos8433@gmail.com>
2026-09-24 09:20:49 +0000

    Remove arquivos antigos do projeto SGE

--- SGE/src/app/components/first-component/first-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-first-component',
-  styleUrl: './first-component.css',
-  templateUrl: './first-component.html',
-})
-export class FirstComponent {}
